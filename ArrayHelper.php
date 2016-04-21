71759f5f840ceeb2fc75cc084647be6a228cc5f2
Igor Chepurnoy <igorzfort@gmail.com>
2016-04-21 21:40:47 +0300

    add the `pluck` function to ArrayHelper

--- ArrayHelper.php
+++ ArrayHelper.php
@@ -304,6 +304,54 @@ class ArrayHelper extends BaseArrayHelper
     }
 
     /**
+     * Pluck an array of values from an array.
+     *
+     * @param  array $array
+     * @param  string|array $value
+     * @param  string|array|null $key
+     * @return array
+     */
+    public static function pluck($array, $value, $key = null)
+    {
+        $results = [];
+
+        list($value, $key) = static::explodePluckParameters($value, $key);
+
+        foreach ($array as $item) {
+            $itemValue = static::getValue($item, $value);
+
+            // If the key is "null", we will just append the value to the array and keep
+            // looping. Otherwise we will key the array using the value of the key we
+            // received from the developer. Then we'll return the final array form.
+            if (is_null($key)) {
+                $results[] = $itemValue;
+            } else {
+                $itemKey = static::getValue($item, $key);
+
+                $results[$itemKey] = $itemValue;
+            }
+        }
+
+        return $results;
+    }
+
+    /**
+     * Explode the "value" and "key" arguments passed to "pluck".
+     *
+     * @param  string|array $value
+     * @param  string|array|null $key
+     * @return array
+     */
+    protected static function explodePluckParameters($value, $key)
+    {
+        $value = is_string($value) ? explode('.', $value) : $value;
+
+        $key = is_null($key) || is_array($key) ? $key : explode('.', $key);
+
+        return [$value, $key];
+    }
+
+    /**
      * @param $value
      * @return mixed
      */
